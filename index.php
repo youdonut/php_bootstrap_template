1af26e8594372db771be3fdce7b1d1c5f2f127bc
youdonut <youdonut@github.com>
2023-02-09 10:34:02 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'assets/inc/meta.php' ?>
+    <title>Document</title>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="assets/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+    
+    <div class="wrapper">
+        <?php include 'assets/inc/navbar.php' ?>
+        <?php include 'assets/inc/sidebar.php' ?>
+
+        <div class="content-wrapper">
+            <section class="content">
+                <h1>Hello World</h1>
+            </section>
+        </div>
+
+
+        <?php include 'assets/inc/footer.php' ?>
+    </div>
+    <?php include 'assets/inc/scripts.php' ?>
+</body>
+
+</html>
